0079c2441592a82a9f50f7437a91451439232638
ehenon <emilienhenon@gmail.com>
2024-05-03 22:49:48 +0200

    feat: better handle appending start

--- src/index.ts
+++ src/index.ts
@@ -19,7 +19,10 @@ const REPLAY_SUBTITLE_FILE_PATH = `${REPLAY_DOWNLOAD_FOLDER}/${REPLAY_SUBTITLE_F
 const PARSED_SUBTITLE_FILE_FOLDER = `${OUTPUT_FOLDER}/2_parsed_subtitles`;
 const PARSED_SUBTITLE_FILE_NAME = 'parsed_vtt';
 const PARSED_SUBTITLE_FILE_PATH = `${PARSED_SUBTITLE_FILE_FOLDER}/${PARSED_SUBTITLE_FILE_NAME}.txt`;
-const AI_OUTPUT_FOLDER = `${OUTPUT_FOLDER}/3_ai_output`;
+const AI_PROMPT_FOLDER = `${OUTPUT_FOLDER}/3_ai_prompt`;
+const AI_PROMPT_FILE_NAME = 'prompt';
+const AI_PROMPT_FILE_PATH = `${AI_PROMPT_FOLDER}/${AI_PROMPT_FILE_NAME}.txt`;
+const AI_OUTPUT_FOLDER = `${OUTPUT_FOLDER}/4_ai_output`;
 const AI_OUTPUT_FILE_NAME = 'ai_response';
 const AI_OUTPUT_FILE_PATH = `${AI_OUTPUT_FOLDER}/${AI_OUTPUT_FILE_NAME}.txt`;
 
@@ -75,20 +78,13 @@ const parseSubtitleFile = async (filePath: string): Promise<string> => {
     const inputFileLines = inputFileContent.split('\n');
 
     let parsedContent = '';
-    let passed34Minutes = false;
     let startAppending = false;
 
-    inputFileLines.forEach((fileLine, currentIndex, originalArray) => {
-      if (fileLine.startsWith('00:34:')) {
-        passed34Minutes = true;
-      }
-      if (passed34Minutes
-        && fileLine.includes('<c.cyan>')
-        && originalArray[currentIndex + 1].trim() === '') {
+    inputFileLines.forEach((fileLine) => {
+      if (fileLine.startsWith('00:33:')) {
         startAppending = true;
       }
-      if (passed34Minutes
-        && startAppending
+      if (startAppending
         && fileLine.trim() !== ''
         && !fileLine.trim().includes('-->')
         && !/^\d+$/.test(fileLine.trim())) {
@@ -109,7 +105,21 @@ const parseSubtitleFile = async (filePath: string): Promise<string> => {
   }
 };
 
-const getChatPromptFromParsedSubtitles = (parsedSubtitles: string): string => `J'ai récupéré un extrait de sous-titres correspondant à la fin d'une émission de jeu télévisé de quiz, à peu près au moment où la présentation des cadeaux des candidats est en cours et où la dernière manche du jeu va bientôt commencer. Cette dernière manche, opposant deux joueurs, consiste à essayer de répondre le plus vite possible à une longue question posée par le présentateur. Lorsqu'une réponse fausse est donnée, la question continue. Lorsqu'une réponse correcte est donnée, le joueur gagne le point et le présentateur continue un peu la question afin de donner tous les éléments d'explication aux téléspectateurs. Avant chaque question, le présentateur annonce un thème, et propose à un candidat de prendre ou de laisser la main en fonction de son attrait pour ce thème. Peux-tu extraire les questions posées par le présentateur en concaténant les bouts de question afin qu'elles soient correctement formatées et ne contiennent pas d'informations superflues ? J'ai besoin que tu présentes ta réponse sous forme de liste de questions, avec les données suivantes : le "libellé" de la question, le "thème", et la "réponse". Voici l'extrait de sous-titres :\n\n\`${parsedSubtitles}\``;
+const generateAIPrompt = async (parsedSubtitles: string): Promise<string> => {
+  try {
+    console.log('* Generating AI prompt...');
+    const prompt = `J'ai récupéré un extrait de sous-titres correspondant à la fin d'une émission de jeu télévisé de quiz, à peu près au moment où la présentation des cadeaux des candidats est en cours et où la dernière manche du jeu va bientôt commencer. Cette dernière manche, opposant deux joueurs, consiste à essayer de répondre le plus vite possible à une longue question posée par le présentateur. Lorsqu'une réponse fausse est donnée, la question continue. Lorsqu'une réponse correcte est donnée, le joueur gagne le point et le présentateur continue un peu la question afin de donner tous les éléments d'explication aux téléspectateurs. Avant chaque question, le présentateur annonce un thème, et propose à un candidat de prendre ou de laisser la main en fonction de son attrait pour ce thème. Peux-tu extraire les questions posées par le présentateur en concaténant les bouts de question afin qu'elles soient correctement formatées et ne contiennent pas d'informations superflues ? J'ai besoin que tu présentes ta réponse sous forme de liste de questions, avec les données suivantes : le "libellé" de la question, le "thème", et la "réponse". Voici l'extrait de sous-titres :\n\n\`${parsedSubtitles}\``;
+    if (!fs.existsSync(AI_PROMPT_FOLDER)) {
+      await fsp.mkdir(AI_PROMPT_FOLDER);
+    }
+    await fsp.writeFile(AI_PROMPT_FILE_PATH, prompt, 'utf-8');
+    console.log('✓ AI prompt successfully generated');
+    return prompt;
+  } catch (err) {
+    console.log(`✗ An error occurred when generating AI prompt: ${err}`);
+    process.exit(1);
+  }
+};
 
 const callAIModel = async (prompt: string): Promise<string> => {
   try {
@@ -145,7 +155,7 @@ const writeAIResponse = async (response: string) => {
   await createOutputFolder();
   await downloadReplayFiles(replayUrl);
   const parsedSubtitles = await parseSubtitleFile(REPLAY_SUBTITLE_FILE_PATH);
-  const chatPrompt = getChatPromptFromParsedSubtitles(parsedSubtitles);
+  const chatPrompt = await generateAIPrompt(parsedSubtitles);
   const aiResponse = await callAIModel(chatPrompt);
   await writeAIResponse(aiResponse);
   process.exit(0);
